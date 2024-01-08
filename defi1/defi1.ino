d65c0b8f6902769efa7279db6f6801c8a28613a9
corslyn <enzomanzinali@protonmail.com>
2024-01-08 13:28:06 +0100

    Movement

--- defi1/defi1.ino
+++ defi1/defi1.ino
@@ -8,9 +8,9 @@ const int numLEDs = 1;
 CRGB leds[numLEDs];
 uint32_t tabColors[] = {0xFF0000, 0xFB4C0D, 0x00FF00};
 
-LightSensor sensorL(A0);
+LightSensor sensorL(A2);
 LightSensor sensorM(A1);
-LightSensor sensorR(A2);
+LightSensor sensorR(A0);
 
 LineDriver driver;
 
@@ -18,12 +18,56 @@ CarMotors engine;
 
 void setup() {
     Serial.begin(9600);
-    
+    engine.init(100);
     Serial.println("Start...");
 }
 
 void loop() {
+
     int state = driver.SetLineDriver(sensorL.detectLine(), sensorM.detectLine(), sensorR.detectLine());
     Serial.println(state);
+    switch(state) {
+      case 0:
+        engine.stop();
+        break;
+      case 1:
+        engine.setSpeed(60);
+        engine.turnLeft();
+                delay(100);
+        engine.goForward();
+
+        break;
+      case 2:
+        engine.setSpeed(100);
+        engine.goForward();
+        break;
+      case 3:
+        engine.setSpeed(60);
+        
+        engine.turnLeft();
+                delay(100);
+        engine.goForward();
+
+        break;
+      case 4:
+        engine.setSpeed(60);
+        engine.turnRight();
+                delay(100);
+        engine.goForward();
+
+        break;
+      case 6:
+        engine.setSpeed(60);
+        engine.turnRight();
+        delay(100);
+                engine.goForward();
+
+        break;
+      case 7:
+        engine.stop();
+        break;
+
+        
+    }
     delay(100);
 }
