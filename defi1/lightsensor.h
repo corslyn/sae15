8965cea924db2cc3fc34c3ab22c6877d56c4357b
corslyn <78977628+corslyn@users.noreply.github.com>
2024-01-14 12:13:46 +0100

    Update lightsensor.h

--- defi1/lightsensor.h
+++ defi1/lightsensor.h
@@ -1,20 +1,22 @@
 #include <Arduino.h>
 
+// Classe pour utiliser un capteur IR
 class LightSensor {
 private:
-    int pin;
+    int pin; // Pin où le capteur est branché
 
 public:
+// Constructeur
     LightSensor(int pinSensor) {
         pin = pinSensor;
+        // lecture analogique du pin
         pinMode(pin, INPUT);
     }
 
-    int detectLine() {
-        int sensorValue = analogRead(pin);
-      
-        // Calibrate these threshold values based on your sensor and lighting conditions
-        if (sensorValue > 600) {
+    int detectLine() { //méthode de détection d'une ligne
+        int sensorValue = analogRead(pin); // lecture de la valeur analogique renvoyée par le capteur IR
+    
+        if (sensorValue > 600) { // réglage de la sensibilité du capteur
             return 1;
         } else {
             return 0;
